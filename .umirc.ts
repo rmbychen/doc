46e96fb2e8e27b4e82bf70d48322532e0268bbd9
chenlei <chenlei@lazyaudio.com>
2022-02-15 18:42:57 +0800

    修改文档访问路径

--- .umirc.ts
+++ .umirc.ts
@@ -8,6 +8,6 @@ export default defineConfig({
   description: 'doc-demo 监控系统文档',
   // locales: [['zh-CN', '中文'], ['en-US', 'English']],
   outputPath: 'docs',
-  publicPath: process.env.NODE_ENV === 'production' ? '/monitor-doc/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/doc/' : '/',
   // more config: https://d.umijs.org/config
 });
